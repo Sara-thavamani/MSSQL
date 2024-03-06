99005c97f2c04600a593ea7f6c9f65aa64e9e95b
Sara-thavamani <saralathavamani2002@gmail.com>
2024-03-06 22:57:07 +0530

    Updated

--- Table2.sql
+++ Table2.sql
@@ -6,3 +6,10 @@ CREATE TABLE [dbo].[Table2] (
     [Grade]  NVARCHAR (10)  NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+INSERT INTO [dbo].[Table2] ([Id], [Name], [Age], [Gender], [Grade])
+VALUES 
+  (1, 'John Doe', 25, 'Male', 'A'),
+  (2, 'Jane Smith', 30, 'Female', 'B'),
+  (3, 'Michael Johnson', 28, 'Male', 'B+'),
+  (4, 'Emily Davis', 22, 'Female', 'A-');
+SELECT * FROM [dbo].[Table2];
