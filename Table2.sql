39205c804b901cbe4946533751d0dd6cdd045572
Sara-thavamani <saralathavamani2002@gmail.com>
2024-03-06 15:03:36 +0530

    first commit

---
+++ Table2.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[Table2] (
+    [Id]     INT            NOT NULL,
+    [Name]   NVARCHAR (100) NOT NULL,
+    [Age]    INT            NOT NULL,
+    [Gender] NVARCHAR (10)  NOT NULL,
+    [Grade]  NVARCHAR (10)  NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
